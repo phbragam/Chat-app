55ba7ce8032b23c398fdd9104093d12a3ee5da33
Pedro Braga <phbm.braga@gmail.com>
2022-07-21 20:53:58 -0300

    web form scene created

---
+++ Assets/Scripts/RetrievePlayers.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RetrievePlayers : MonoBehaviour
+{
+    public void GetDetails()
+    {
+        StartCoroutine(RetrievePlayerDetails());
+    }
+
+    IEnumerator RetrievePlayerDetails()
+    {
+        Debug.Log("Retrieving Details");
+
+        WWWForm form = new WWWForm();
+        WWW www = new WWW("https://jrm-project-server.000webhostapp.com/getplayers.php", form);
+
+        yield return www;
+        if (www.error != null)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            Debug.Log(www.text);
+        }
+    }
+}
